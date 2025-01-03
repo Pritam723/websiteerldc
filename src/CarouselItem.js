1807adfa2b3138e1f60afbb4717fb11b728ab6a7
ritik-coder <ritikpal41@gmail.com>
2025-01-03 12:26:07 +0530

    use for next development

--- src/CarouselItem.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function CarouselItem({ index, imgUrl, imgTitle }) {
-  return (
-    <div key={index} className="carousel-card">
-      <img src={imgUrl} alt={imgTitle}></img>
-    </div>
-  );
-}
