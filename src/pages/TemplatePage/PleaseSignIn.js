1b0630fb369d6a5d7c652f4d8e2f089b6b75bb66
Munesh <muneshjnv95@gmail.com>
2025-02-13 17:24:26 +0530

    Restricted access

--- src/pages/TemplatePage/PleaseSignIn.js
+++ src/pages/TemplatePage/PleaseSignIn.js
@@ -1,18 +1,37 @@
 import React from "react";
 import BaseLayout from "layouts/sections/components/BaseLayout";
-// import SignIn from "pages/FunctionalPages/Auth/SignIn";
 import { useNavigate } from "react-router-dom";
+import { Button } from "primereact/button";
+import { Card } from "primereact/card";
+import { Divider } from "primereact/divider";
+import { Message } from "primereact/message";
+import Restricted_access from 'assets/images/Restricted_access.png';
 
 export default function PleaseSignIn({ breadcrumb, redirectionURL }) {
   const navigate = useNavigate();
+  
   const handleRedirect = () => {
-    navigate("/user/signIn", { state: { redirectionURL: redirectionURL } });
+    navigate("/user/signIn", { state: { redirectionURL } });
   };
+
   return (
-    <BaseLayout title={"Please Sign In"} breadcrumb={breadcrumb}>
-      Please Sign in.
-      <br />
-      <button onClick={handleRedirect}>Go to Sign In Page</button>
+    <BaseLayout breadcrumb={breadcrumb}>
+      <Card className="shadow-3 p-5 text-center w-full max-w-md">
+        <img 
+          src={Restricted_access} 
+          alt="Restricted Access"
+          className="w-2 mx-auto"
+        />
+        <Divider />
+        <h1 severity="warn">You need to sign in to view this page</h1>
+        {/* <Message severity="warn" text="You need to sign in to view this page" /> */}
+        <div className="mt-4">
+          <Button 
+            label="Go to Sign In Page" 
+            onClick={handleRedirect} 
+          />
+        </div>
+      </Card>
     </BaseLayout>
   );
 }
