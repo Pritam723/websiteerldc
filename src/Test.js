5efdb0ae0c42582745223bf67a429e39d1932e8d
PRITAM DUTTA <00091@erldc.net>
2025-03-04 09:19:57 +0530

    04.03.2025

--- src/Test.js
+++ src/Test.js
@@ -1,13 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import BaseLayout from "layouts/sections/components/BaseLayout";
-import { useNavigate } from "react-router-dom";
-import { Button } from "primereact/button";
 import { Card } from "primereact/card";
-import { Divider } from "primereact/divider";
-import { Message } from "primereact/message";
-import { ExclamationTriangleIcon } from "primereact/icons/exclamationtriangle";
-import Restricted_access from "assets/images/Restricted_access.gif";
-import { max } from "moment";
 
 /**
  * PleaseSignIn Component
@@ -18,63 +11,96 @@ import { max } from "moment";
  * @param {string} props.redirectionURL - URL to redirect to after sign-in
  */
 export default function PleaseSignIn({ breadcrumb, redirectionURL }) {
-  const navigate = useNavigate();
-
-  /**
-   * Redirects the user to the sign-in page with the redirection URL in state.
-   */
-  const handleRedirect = () => {
-    navigate("/user/signIn", { state: { redirectionURL } });
-  };
+  const [isExpanded, setIsExpanded] = useState(false);
 
   return (
-    <BaseLayout breadcrumb={breadcrumb}>
+    <BaseLayout breadcrumb={[]}>
       <Card className="shadow-3 p-5 text-center w-full max-w-md mx-400">
-        <div>
-          <h1 style={{ color: "#cc8925" }}>RESTRICTED ACCESS</h1>
-        </div>
+        <strong>RTI</strong> or <strong>Right to Information Act</strong> is an
+        initiative that was taken in 1995 by the Department of Personnel and
+        Training, Ministry of Personnel, Public Grievances and Pensions as a
+        step forward to ensure the citizen of India have access to Government
+        information. Every individual can ask for any information and they are
+        rightful to receive a timely response. This is a major step which aims
+        to promote transparency in government institutions in India. This
+        section of ERLDC website along with other relevant sections contains
+        information as required to be published under Section 4 (1) (b) of the
+        Act. Compliance under Section 4 (1)(b) of Right to Information Act, 2005
         <br />
-        <img
-          src={Restricted_access}
-          alt="Restricted Access"
-          className="w-2 mx-auto mb-4 rounded-full"
-          height={max}
-          width={max}
-        />
-        {/* <Divider /> */}
-        {/* <div><Message severity="warn" text="You need to sign in to view this page" /></div> */}
-        <div>
-          <Message
-            severity="warn"
-            content={
-              <span
-                style={{
-                  fontSize: "1.5rem",
-                  display: "flex",
-                  alignItems: "center",
-                }}
-              >
-                <ExclamationTriangleIcon
-                  style={{
-                    width: "1.5rem",
-                    height: "1.5rem",
-                    marginRight: "0.5rem",
-                  }}
-                />
-                You need to sign in to view this page
-              </span>
-            }
-          />
-        </div>
-        <div className="mt-4">
-          <Button
-            label="Sign In to Continue"
-            style={{ backgroundColor: "#6366f1", borderColor: "#6366f1" }}
-            icon="pi pi-sign-in"
-            className="p-button-warning p-button-rounded p-button-lg"
-            onClick={handleRedirect}
-          />
-        </div>
+        <a
+          className="btn btn-primary"
+          href="https://erldc.in/wp-content/uploads/2018/07/Information_Available_in_Electronic_Form.pdf"
+          target="_blank"
+          rel="noopener"
+        >
+          INFORMATION AVAILABLE IN ELECTRONIC FORM
+        </a>
+        <br />
+        <a
+          className="btn btn-primary"
+          role="button"
+          href="https://erldc.in/wp-content/uploads/2024/04/Emp-List-04.04.2024.pdf"
+          target="_blank"
+          rel="noopener"
+        >
+          A DIRECTORY OF ITS OFFICERS EMPLOYEES.
+        </a>
+        <br />
+        <a
+          className="btn btn-primary"
+          role="button"
+          href="https://erldc.in/wp-content/uploads/2019/07/THE-NAMES-DESIGNATIONS-OTHER-PARTICULARS-OF-THE-PUBLIC-INFORMATION-OFFICERS-04th-july.pdf"
+          target="_blank"
+          rel="noopener"
+        >
+          THE NAMES, DESIGNATIONS AND OTHER PARTICULARS OF THE PUBLIC
+          INFORMATION OFFICERS.
+        </a>
+        <br />
+        <button
+          className="btn btn-primary"
+          onClick={() => setIsExpanded(!isExpanded)}
+        >
+          REQUEST AND REPLY UNDER RTI ACT 2005
+        </button>
+        {isExpanded && (
+          <div className="mt-3">
+            <a
+              href="https://erldc.in/wp-content/uploads/2018/07/RTI-REQUEST-REPLY-2016-17.pdf"
+              target="_blank"
+              rel="noopener"
+              className="d-block"
+            >
+              RTI REQUEST REPLY 2016-17
+            </a>
+            <a
+              href="https://erldc.in/wp-content/uploads/2018/07/RTI-REQUEST-REPLY-2017-18.pdf"
+              target="_blank"
+              rel="noopener"
+              className="d-block"
+            >
+              RTI REQUEST REPLY 2017-18
+            </a>
+            <a
+              href="https://erldc.in/wp-content/uploads/2019/07/RTI-Monthly-Report-2018-19.pdf"
+              target="_blank"
+              rel="noopener"
+              className="d-block"
+            >
+              RTI REQUEST REPLY 2018-19
+            </a>
+            <a
+              href="https://erldc.in/wp-content/uploads/2019/07/RTI-Monthly-Report-2019-20.pdf"
+              target="_blank"
+              rel="noopener"
+              className="d-block"
+            >
+              RTI REQUEST REPLY 2019-20
+            </a>
+          </div>
+        )}
+        <div style={{ height: 220, width: 100, display: "block" }}></div>
+        &nbsp;
       </Card>
     </BaseLayout>
   );
